e3142a159238921f2eb2bec0459a2ee261f7672f
HabibaElmahdy <habibaelmahdy26@gmail.com>
2023-04-25 20:10:09 -0700

    26/4

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * struct specifiers - describe a particular type of struct
+ * @specifier: the operator
+ * @f: the pointer of the function
+ */
+int _write(char c);
+int _printf(const char *format, ...);
+#endif
---
+++ printf.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * _printf - improvised printf function
+ * @format: string format to be printed
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+va_list args;
+
+va_start(args, format);
+
+int count = 0;
+
+while (*format != 0)
+{
+if (*format == %)
+{
+format++;
+switch (*format)
+{
+case c:
+{
+char c = (char) va_arg(args, int);
+
+fwrite(c);
+count++;
+break;
+}
+case s:
+{
+char s = (char) va_arg(args, char*);
+
+while (s != 0)
+{
+write(*s);
+count++;
+s++;
+}
+break;
+}
+case %:
+{
+write(%);
+count++;
+break;
+}
+default:
+break;
+}
+}
+else
+{
+write(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
+
+}
